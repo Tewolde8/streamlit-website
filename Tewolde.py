aad24a4bc64659acac4216768244336818811f4d
Tewolde8 <issac.tewolde88@gmail.com>
2024-12-02 15:24:51 -0500

    Update Tewolde.py

--- Tewolde.py
+++ Tewolde.py
@@ -2,7 +2,6 @@
 
 import streamlit as st
 from PIL import Image
-import cv2
 
 #Move set_page_config to the top
 st.set_page_config(page_title="Caffeine Production", page_icon=":trees:", layout="wide")
